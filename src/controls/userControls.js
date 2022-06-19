3ae5c4365b15f8ad7caba8669127ed56afeeb82f
CalebAurand <caurand13@gmail.com>
2022-06-18 22:54:10 -0500

    added basic functionality of login, need to add json web token functionality next

--- src/controls/userControls.js
+++ src/controls/userControls.js
@@ -24,7 +24,7 @@ const register = async (req, res) =>{
   let sql = "INSERT INTO usersWithPasswords (first_name, last_name, email, pw_hash) VALUES(?, ?, ?, ?);";
 
   //run the database query to register/create the new user
-  db.query(sql, params, /**middleware here */ (err, results)=>{
+  db.query(sql, params, (err, results)=>{
     if(err){
       console.log("Could not add user ", err);
       res.sendStatus(500);
@@ -37,8 +37,40 @@ const register = async (req, res) =>{
   //
 };
 
-const login = (req, res) =>{
-
+const login = async (req, res) =>{
+  console.log('login');
+  //hold their input email and use it for the find/filter criteria for the db query
+  let email = req.body.email;
+  //hold their input password for comparison
+  let password = req.body.password;  
+  //create a variable dbPWHash to hold the password hash found by the db query
+  let dbPWHash;
+  //set param for email for finding the password hash in the database for comparison
+  let params = [email];
+  //set sql statement to find the password hash for the corresponding email
+  let sql = "SELECT pw_hash FROM usersWithPasswords WHERE email = ?;";
+  db.query(sql, params, async (err, results)=>{
+    if(err){
+      res.sendStatus(500);
+    } else {
+      if(results.length > 1){
+        console.log("returned more than 1 result");
+        res.sendStatus(500);
+        return;
+      } else if(results.length < 1){
+        res.sendStatus(404);
+      } else {
+        dbPWHash = results[0].pw_hash;
+      }
+    }
+    //still in query compare pw-hashes
+    if(await argon.verify(dbPWHash, password)){
+      res.sendStatus(200);
+    } else {
+      console.log("password couldn't be verified");
+      res.sendStatus(400);
+    }
+  });
 };
 
 
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -4,4 +4,6 @@ let route = new express.Router();
 
 route.post("/register", controller.register);
 
+route.post("/login", controller.login);
+
 module.exports = route;
